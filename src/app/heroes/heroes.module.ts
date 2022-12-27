d437898e63fb46e9bf6abaa8a83023e98bbbaf09
txv.dev <txv.dev@gmail.com>
2022-12-27 13:51:10 +0100

    include angular material and configure navbar

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
@@ -22,6 +23,7 @@ import { ListadoComponent } from './pages/listado/listado.component';
   ],
   imports: [
     CommonModule,
+    MaterialModule,
     HeroesRoutingModule
   ]
 })
