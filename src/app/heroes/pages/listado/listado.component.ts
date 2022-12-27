22179accb423b12cc86450153e93ae2494d8404a
txv.dev <txv.dev@gmail.com>
2022-12-27 16:46:42 +0100

    create heroServices and import httpClientModule

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class ListadoComponent {
 
+  constructor( private heroService: HeroesService ) {
+
+  }
+
+  ngOnInit(): void{
+    this.heroService.getHeroes()
+      .subscribe( resp => console.log(resp))
+  }
 }
